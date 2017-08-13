8ad5862f8795d043782eb772d043009f5a8db760
Zumo <ZDDM@users.noreply.github.com>
2017-08-14 00:20:11 +0200

    Update and rename config.php.dist to config.php

--- config.php
+++ config.php
@@ -4,11 +4,11 @@ class Config
 {
     const ROOT_PATH = ROOT_PATH;
 
-    const WEBROOT_PATH = "/htdocs";
+    const WEBROOT_PATH = "/media";
     const CACHE_PATH = "/cache";
     const POOL_PATH = '/files';
 
-    const ROOT_URL = "http://sub.domain.tld";
+    const ROOT_URL = "http://yourwebsitegoeshere.moe/media";
     const API_KEY = "SECRET DO NOT SHARE";
 
     public static function getWebRoot()
